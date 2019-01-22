3bab98de2e3246c89c1854e7bcdff515348445fe
xingqiangpeng <873871674@qq.com>
2019-01-22 10:13:15 +0800

    邢强鹏  考生页面 登录页面  个人信息页面完成

--- src/main/java/com/four/exam/repository/StudentRepository.java
+++ src/main/java/com/four/exam/repository/StudentRepository.java
@@ -11,4 +11,6 @@ public interface StudentRepository extends JpaRepository<Student,Integer> {
     List<Student> findBySdepLike(String str);
     List<Student> findBySnameLike(String str);
     List<Student> findBySnameLikeAndScreatdateBetween(String str1,String str2,String str3);
+    List<Student> findBySnumberAndSpassword(String snumber,String spassword);
+
 }
